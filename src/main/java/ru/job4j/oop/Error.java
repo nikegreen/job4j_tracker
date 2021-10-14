bf3ece6936b2236347c25bd434fbad0cef6f3e11
nike z <nikegreen@yandex.ru>
2021-10-14 21:52:01 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,36 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+        this.active = false;
+        this.status = 0;
+        this.message = "";
+    }
+
+    public void info() {
+        System.out.println("active=" + this.active
+                + " status=" + this.status
+                + " message=" + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error errDefault = new Error();
+        Error err1 = new Error(true, 1, "err 1");
+        Error err2 = new Error(false, 2, "err 2");
+        Error err3 = new Error(true, 3, "err 3");
+        errDefault.info();
+        err1.info();
+        err2.info();
+        err3.info();
+    }
+}
