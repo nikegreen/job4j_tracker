7b2e122ebf7a750a68db0b120b8e176d2f5b5fe5
nike z <nikegreen@yandex.ru>
2021-10-14 20:50:29 +0500

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,9 +1,33 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String name;
+
+    private String food;
+
+    public void giveNick(String nick) {
+        name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " eat " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
-
