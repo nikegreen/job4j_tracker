a7d6cf2e19a8dad20e07e4064411592e0424391b
nike z <nikegreen@yandex.ru>
2021-10-17 13:54:46 +0500

    2.2. Рефакторинг - Площадь треугольника. [#122642 #216846] уменьшена точность, исправлены ошибки стиля.

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -8,16 +8,16 @@ public class TriangleTest {
 
     @Test
     public void semiPerimeter() {
-        Point p1 = new Point(0,0);
+        Point p1 = new Point(0, 0);
         Point p2 = new Point(0, 2);
         Point p3 = new Point(2, 0);
         Triangle t = new Triangle(p1, p2, p3);
-        assertEquals(3.414213562373095, t.semiPerimeter(p1.distance(p2), p1.distance(p3), p2.distance(p3)), 0.00001);
+        assertEquals(3.41421, t.semiPerimeter(p1.distance(p2), p1.distance(p3), p2.distance(p3)), 0.001);
     }
 
     @Test
     public void existTrue() {
-        Point p1 = new Point(0,0);
+        Point p1 = new Point(0, 0);
         Point p2 = new Point(0, 2);
         Point p3 = new Point(2, 0);
         Triangle t = new Triangle(p1, p2, p3);
@@ -26,7 +26,7 @@ public class TriangleTest {
 
     @Test
     public void existFalse() {
-        Point p1 = new Point(0,0);
+        Point p1 = new Point(0, 0);
         Point p2 = new Point(0, 2);
         Point p3 = new Point(0, 1);
         Triangle t = new Triangle(p1, p2, p3);
@@ -35,10 +35,10 @@ public class TriangleTest {
 
     @Test
     public void area() {
-        Point p1 = new Point(0,0);
+        Point p1 = new Point(0, 0);
         Point p2 = new Point(0, 2);
         Point p3 = new Point(2, 0);
         Triangle t = new Triangle(p1, p2, p3);
-        assertEquals(2, t.area(), 0.00001);
+        assertEquals(2, t.area(), 0.001);
     }
 }
