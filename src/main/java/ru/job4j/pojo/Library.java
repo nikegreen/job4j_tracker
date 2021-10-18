5ae73c9228f24a4caa00ce061033e81a7810e9b5
nike z <nikegreen@yandex.ru>
2021-10-18 15:15:18 +0500

    3. Массивы и модели. [#174892 #217468] исправлено.

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -7,18 +7,18 @@ public class Library {
         Book book3 = new Book("book3", 10);
         Book book4 = new Book("Clean code", 22);
         Book[] books = new Book[]{book1, book2, book3, book4};
-        for (int i = 0; i < books.length; i++) {
-            System.out.println(books[i].getName());
+        for (Book book : books) {
+            System.out.println(book.getName());
         }
         Book tmp = books[0];
         books[0] = books[3];
         books[3] = tmp;
-        for (int i = 0; i < books.length; i++) {
-            System.out.println(books[i].getName());
+        for (Book book : books) {
+            System.out.println(book.getName());
         }
-        for (int i = 0; i < books.length; i++) {
-            if (books[i].getName().equals("Clean code")) {
-                System.out.println(books[i].getName());
+        for (Book book : books) {
+            if ("Clean code".equals(book.getName())) {
+                System.out.println(book.getName());
             }
         }
     }
