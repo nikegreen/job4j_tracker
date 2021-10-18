15f9d46bf597813ddbcfee5f0e73fa654ba80e55
nike z <nikegreen@yandex.ru>
2021-10-19 01:11:56 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
+
     public static void main(String[] args) {
         Item item = new Item(1, "item");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
