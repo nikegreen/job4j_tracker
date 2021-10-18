8155c95887b73ba1c6f3eaf73970c16cb7f214a6
nike z <nikegreen@yandex.ru>
2021-10-19 01:10:59 +0500

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,6 @@ public class StartUI {
         Item item = new Item(1, "item");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(item.getCreated().format(formatter));
+        System.out.println(item);
     }
 }
