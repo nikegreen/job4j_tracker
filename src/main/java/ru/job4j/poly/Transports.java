1f077b5d9ceae704ee90bed10722271003fedaff
nike z <nikegreen@yandex.ru>
2021-10-19 17:04:28 +0500

    0. Что такое исключение. [#3821]

--- src/main/java/ru/job4j/poly/Transports.java
+++ src/main/java/ru/job4j/poly/Transports.java
@@ -2,9 +2,13 @@ package ru.job4j.poly;
 
 public class Transports {
     public static void main(String[] args) {
-        Vehicle[] vehicles = new Vehicle[] {new Airplan(), new Train(), new Autobus(), new Train(), new Autobus() };
-        for (Vehicle venicle : vehicles) {
-            venicle.move();
+        Vehicle[] vehicles = new Vehicle[] {new Airplan(),
+                new Train(),
+                new Autobus(),
+                new Train(),
+                new Autobus() };
+        for (Vehicle current: vehicles) {
+            current.move();
         }
     }
 }
