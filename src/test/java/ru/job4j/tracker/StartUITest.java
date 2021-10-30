e1eb8305e20c36174c073fc327e94164ba1e76b0
nike z <nikegreen@yandex.ru>
2021-10-31 00:37:20 +0500

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -221,4 +221,27 @@ public class StartUITest {
                         + "=== Exit Program ===" + ln
         ));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitProgramAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                        "Menu:" + ln
+                                + "0. Exit Program" + ln
+                                + "Wrong input, you can select: 0 .. " + (actions.length - 1) + ln
+                                + "Menu:" + ln
+                                + "0. Exit Program" + ln
+                                + "=== Exit Program ===" + ln
+                )
+        );
+    }
 }
