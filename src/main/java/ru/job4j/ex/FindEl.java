080e626ca22af35158046ff64e21a389de077074
nike z <nikegreen@yandex.ru>
2021-10-31 17:02:41 +0500

    0.4. Пользовательские исключения. [#219367 #222579] исправлено. Убрал int rsl=-1; В классе ElementNotFoundException убрал java.lang.

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,7 +2,6 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
         for (int i = 0; i < value.length; i++) {
             if (value[i] != null && value[i].equals(key)) {
                 return i;
