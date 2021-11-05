044f21d419e41063b32617c3021bf7cccecbdac2
nike z <nikegreen@yandex.ru>
2021-11-05 23:29:09 +0500

    6. Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -8,16 +8,6 @@ import static org.junit.Assert.assertThat;
 
 public class PhoneDictionaryTest {
     @Test
-    public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
-        phones.add(
-                new Person("Petr", "Arsentev", "534872", "Bryansk")
-        );
-        ArrayList<Person> persons = phones.find("Petr");
-        assertThat(persons.get(0).getSurname(), is("Arsentev"));
-    }
-
-    @Test
     public void whenNotFindByName() {
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(
@@ -27,4 +17,13 @@ public class PhoneDictionaryTest {
         assertThat(persons.size(), is(0));
     }
 
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
 }
