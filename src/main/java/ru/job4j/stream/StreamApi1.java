2cc10d936619fc7d8d43801cd7318bf6e0ddae0f
nike z <nikegreen@yandex.ru>
2021-11-06 15:42:11 +0500

    0. Stream API [#58166 #225053] знак в лямбде ">".

--- src/main/java/ru/job4j/stream/StreamApi1.java
+++ src/main/java/ru/job4j/stream/StreamApi1.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class StreamApi1 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 3, -3, 4, -1, 2);
-        List<Integer> out = list.stream().filter(i -> i >= 0).collect(Collectors.toList());
+        List<Integer> out = list.stream().filter(i -> i > 0).collect(Collectors.toList());
         System.out.println(out);
     }
 }
