556f46de31d19dc15fa09c341f4635bd7bf36b94
nike z <nikegreen@yandex.ru>
2021-11-06 15:31:40 +0500

    4. Сортировка номера [#221284 #224596] убрал обработку подпунктов и обработку числового формата.

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -9,26 +9,6 @@ public class LexSort implements Comparator<String> {
     public int compare(String left, String right) {
         String[] l = left.split("\\.");
         String[] r = right.split("\\.");
-        int lc = 0;
-        int rc = 0;
-        while (lc < l.length && rc < r.length) {
-            int li, ri;
-            try {
-                li = Integer.parseInt(l[lc]);
-                lc++;
-            } catch (NumberFormatException e) {
-                break;
-            }
-            try {
-                ri = Integer.parseInt(r[rc]);
-                rc++;
-            } catch (NumberFormatException e) {
-                break;
-            }
-            if (li != ri) {
-                return Integer.compare(li, ri);
-            }
-        }
-        return Integer.compare(lc, rc);
+        return Integer.compare(Integer.parseInt(l[0]), Integer.parseInt(r[0]));
     }
 }
