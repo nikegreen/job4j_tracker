9c37a0020d30cc9864c65194fecd9686c4b7aa5a
nike z <nikegreen@yandex.ru>
2021-11-07 16:19:56 +0500

    3. Уникальность элементов и сортировка. [#110225 #225309] исправлено.

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,7 +8,7 @@ public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .sorted((l, r) -> l.getCity().compareTo(r.getCity()))
+                .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
     }
