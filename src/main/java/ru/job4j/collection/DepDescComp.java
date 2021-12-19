cda4abfab66c4707a4a8fe836a13ae4e8108a2fd
nike z <nikegreen@yandex.ru>
2021-12-19 21:18:57 +0500

    2. Отсортировать департаменты [#34352 #239206] исправлен компаратор.

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,6 +5,17 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        return o1.compareTo(o2);
+        String[] oo1 = o1.split("/");
+        String[] oo2 = o2.split("/");
+        int len = Math.min(oo1.length, oo2.length);
+        int count = 0;
+        while (count < len) {
+            int res = oo1[count].compareTo(oo2[count]);
+            if (res != 0) {
+                return res;
+            }
+            count++;
+        }
+        return Integer.compare(oo1.length, oo2.length);
     }
 }
