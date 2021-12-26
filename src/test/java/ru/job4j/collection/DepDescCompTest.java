e66e734ccd5fbc21405ecf8491dfe8c22d8ce75a
nike z <nikegreen@yandex.ru>
2021-12-26 20:44:05 +0500

    2. Отсортировать департаменты [#34352 #239206] исправлены тесты.

--- src/test/java/ru/job4j/collection/DepDescCompTest.java
+++ src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -1,12 +1,12 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 
 public class DepDescCompTest {
-
     @Test
     public void compare() {
         int rsl = new DepDescComp().compare(
@@ -25,4 +25,21 @@ public class DepDescCompTest {
         assertThat(rsl, lessThan(0));
     }
 
+    @Test
+    public void whenFirstDepartmentIsDifferentThenAsc() {
+        int rsl = new DepDescComp().compare(
+                "K2/SK1/SSK1",
+                "K1/SK1/SSK2"
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenFirstDepartmentIsDifferentThenDesc() {
+        int rsl = new DepDescComp().compare(
+                "K1/SK1/SSK1",
+                "K2/SK1"
+        );
+        assertThat(rsl, greaterThan(0));
+    }
 }
