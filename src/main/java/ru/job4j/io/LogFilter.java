102d1148fe4f85acb21c479bcf5113c3091743d6
nike z <nikegreen@yandex.ru>
2022-02-02 20:59:07 +0500

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> lines = null;
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            lines = in.lines()
+                    .filter(value -> {
+                        boolean res = false;
+                        String[] str = value.split(" ");
+                        if (str.length > 1) {
+                            res = "404".equals(str[str.length - 2]);
+                        }
+                        return res;
+                    })
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        log.forEach(System.out::println);
+    }
+}
