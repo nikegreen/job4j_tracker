55d7b8d073251a8acac09d3b994b3c11d32eefce
nike z <nikegreen@yandex.ru>
2022-02-03 22:32:04 +0500

    2. Анализ доступности сервера. [#859 #259625]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,61 +1,38 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        class InputLog {
-            private final boolean available;
-            private final String time;
-
-            public InputLog(String str) {
-                String[] s = str.split(" ");
-                int res = switch (Integer.valueOf(s[0])) {
-                    case 200 -> 1;
-                    case 300 -> 1;
-                    case 400 -> -1;
-                    case 500 -> -1;
-                    default -> 0;
-                };
-                available = res == 1;
-                time = s[1];
-            }
-
-            public boolean isAvailable() {
-                return available;
-            }
-
-            public String getTime() {
-                return time;
-            }
-        }
-
-        List<InputLog> in = new  ArrayList<>();
-        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            read.lines().forEach(val -> {
-                if (val.length() == 12) {
-                   in.add(new InputLog(val));
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            Iterator<InputLog> inIterator = in.iterator();
-            InputLog inputLog;
-            while (inIterator.hasNext()) {
-                do {
-                    inputLog = inIterator.next();
-                } while (inIterator.hasNext() && inputLog.isAvailable());
-                String begTime = inputLog.getTime();
-                do {
-                    inputLog = inIterator.next();
-                } while (inIterator.hasNext() && !inputLog.isAvailable());
-                String endTime = inputLog.getTime();
-                out.println(begTime + ";" + endTime);
+            try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+                String val;
+                boolean oldAvailable = true;
+                String begTime = "";
+                while ((val = read.readLine()) != null) {
+                    val = val.trim();
+                    int index = val.indexOf(' ');
+                    String err;
+                    String time;
+                    if (val.length() > 3
+                            && index > 0
+                            && index < val.length() - 1
+                    ) {
+                        err = val.substring(0, index);
+                        time = val.substring(index + 1);
+                        boolean available = "200".equals(err) || "300".equals(err);
+                        if (oldAvailable != available) {
+                            if (available) {
+                                out.println(begTime + ";" + time);
+                            } else {
+                                begTime = time;
+                            }
+                            oldAvailable = available;
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         } catch (Exception e) {
             e.printStackTrace();
