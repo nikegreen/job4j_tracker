e0f9298cc0ff86759ed31100ecef11c845b46cef
nike z <nikegreen@yandex.ru>
2022-02-04 23:06:47 +0500

    2. Анализ доступности сервера. [#859 #259625]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -5,34 +5,31 @@ import java.io.*;
 public class Analizy {
     public void unavailable(String source, String target) {
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-                String val;
-                boolean oldAvailable = true;
-                String begTime = "";
-                while ((val = read.readLine()) != null) {
-                    val = val.trim();
-                    int index = val.indexOf(' ');
-                    String err;
-                    String time;
-                    if (val.length() > 3
-                            && index > 0
-                            && index < val.length() - 1
-                    ) {
-                        err = val.substring(0, index);
-                        time = val.substring(index + 1);
-                        boolean available = "200".equals(err) || "300".equals(err);
-                        if (oldAvailable != available) {
-                            if (available) {
-                                out.println(begTime + ";" + time);
-                            } else {
-                                begTime = time;
-                            }
-                            oldAvailable = available;
+            BufferedReader read = new BufferedReader(new FileReader(source));
+            String val;
+            boolean oldAvailable = true;
+            String begTime = "";
+            while ((val = read.readLine()) != null) {
+                val = val.trim();
+                int index = val.indexOf(' ');
+                String err;
+                String time;
+                if (val.length() > 3
+                        && index > 0
+                        && index < val.length() - 1
+                ) {
+                    err = val.substring(0, index);
+                    time = val.substring(index + 1);
+                    boolean available = "200".equals(err) || "300".equals(err);
+                    if (oldAvailable != available) {
+                        if (available) {
+                            out.println(begTime + ";" + time);
+                        } else {
+                            begTime = time;
                         }
+                        oldAvailable = available;
                     }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } catch (Exception e) {
             e.printStackTrace();
