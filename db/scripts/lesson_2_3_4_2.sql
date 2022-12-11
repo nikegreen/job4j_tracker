789b0ac9efc3dddc32c9ca4ad2c15427770152db
nikez <nikegreen@yandex.ru>
2022-12-11 23:45:18 +0500

    2. Триггеры [#504803] - 2

--- db/scripts/lesson_2_3_4_2.sql
+++ db/scripts/lesson_2_3_4_2.sql
@@ -13,33 +13,6 @@ create table history_of_price (
     date timestamp
 );
 
-create trigger discount_trigger
-    after insert
-    on products
-    for each row
-    execute procedure discount();
-
-create or replace function discount()
-    returns trigger as
-$$
-    BEGIN
-        update products
-        set price = price - price * 0.2
-        where count <= 5 AND id = new.id;
-        return NEW;
-    END;
-$$
-LANGUAGE 'plpgsql';
-
-insert into products (name, producer, count, price) VALUES ('product_3', 'producer_3', 8, 115);
-
-insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 50);
-
-alter table products disable trigger discount_trigger;
-
-insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 50);
-
-drop trigger discount_trigger on products;
 
 create or replace function statement_tax()
     returns trigger as
@@ -85,7 +58,7 @@ CREATE OR REPLACE FUNCTION log_history_of_price()
 $$
 BEGIN
     INSERT INTO history_of_price(name, price, date)
-    VALUES(OLD.name, OLD.price, now());
+    VALUES(new.name, new.price, now());
 	RETURN NEW;
 END;
 $$
@@ -96,3 +69,11 @@ CREATE TRIGGER last_name_changes
   ON products
   FOR EACH ROW
   EXECUTE PROCEDURE log_history_of_price();
+
+
+insert into products (name, producer, count, price) VALUES ('product_3', 'producer_3', 8, 115);
+
+insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 50);
+
+insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 50);
+
