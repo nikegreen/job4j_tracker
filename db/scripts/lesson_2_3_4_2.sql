c5e99c9ec66b7d67f5ae2e27da4b1034c0694bb6
nikez <nikegreen@yandex.ru>
2022-12-11 23:35:21 +0500

    2. Триггеры [#504803]

---
+++ db/scripts/lesson_2_3_4_2.sql
@@ -0,0 +1,98 @@
+create table products (
+    id serial primary key,
+    name varchar(50),
+    producer varchar(50),
+    count integer default 0,
+    price integer
+);
+
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
+
+create trigger discount_trigger
+    after insert
+    on products
+    for each row
+    execute procedure discount();
+
+create or replace function discount()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price - price * 0.2
+        where count <= 5 AND id = new.id;
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+insert into products (name, producer, count, price) VALUES ('product_3', 'producer_3', 8, 115);
+
+insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 50);
+
+alter table products disable trigger discount_trigger;
+
+insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 50);
+
+drop trigger discount_trigger on products;
+
+create or replace function statement_tax()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price + price * 0.2
+        where id = new.id;
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger statement_tax_trigger
+    after insert
+    on products
+    for each statement
+    execute procedure statement_tax();
+
+
+
+create or replace function row_tax()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price + price * 0.2
+        where id = new.id;
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger row_tax_trigger
+    before insert
+    on products
+    for each row
+    execute procedure row_tax();
+
+
+CREATE OR REPLACE FUNCTION log_history_of_price()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+    INSERT INTO history_of_price(name, price, date)
+    VALUES(OLD.name, OLD.price, now());
+	RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER last_name_changes
+  BEFORE INSERT
+  ON products
+  FOR EACH ROW
+  EXECUTE PROCEDURE log_history_of_price();
